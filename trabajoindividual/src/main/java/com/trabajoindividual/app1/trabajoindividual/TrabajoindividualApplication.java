04578471e49ae6ed6022fa66659411e0b42e1914
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-15 21:13:43 -0500

    New proyecto of practica

---
+++ trabajoindividual/src/main/java/com/trabajoindividual/app1/trabajoindividual/TrabajoindividualApplication.java
@@ -0,0 +1,13 @@
+package com.trabajoindividual.app1.trabajoindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoindividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoindividualApplication.class, args);
+	}
+
+}
