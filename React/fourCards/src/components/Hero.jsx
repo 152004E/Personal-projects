23912552de6cadcb55ce5eb522bd7a63610491e9
Emerson Reyes <reyesemerson643@gmail.com>
2025-10-19 22:04:17 -0500

    hero

--- React/fourCards/src/components/Hero.jsx
+++ React/fourCards/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <header className="text-center border-2 flex flex-col justify-center items-center py-5 px-3 rounded-2xl shadow-2xl mb-[60px]
-    transition duration-[1s] hover:scale-[1.07] cursor-pointer
+    transition duration-[1s] hover:scale-[1.07] cursor-pointer hover:shadow-none
     ">
   <h1 className="text-2xl font-extralight">
     Entrega confiable y eficiente
