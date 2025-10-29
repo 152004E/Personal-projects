9060d1c1446f30369d51e0246eeb7eabd13315f0
Emerson Reyes <reyesemerson643@gmail.com>
2025-10-29 17:55:46 -0500

    Adelanto formulario

--- React/formularioReact/src/components/Dashboard.jsx
+++ React/formularioReact/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
-import logo from "../assets/images/imgFormly.png";
+import logo from "../assets/images/inv.png";
 export const Dashboard = () => {
   return (
-    <img className="w-[320px] m-auto mb-[120px]" src={logo} alt="Dashboard" />
+    <img className="w-[320px] m-auto mb-6 lg:w-[400px]" src={logo} alt="Dashboard" />
   )
 }
 
