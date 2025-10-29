bc60b997c1bf13c303f025bb7f783a78e4d1c441
Emerson Reyes <reyesemerson643@gmail.com>
2025-10-28 22:10:39 -0500

    Instalacion tailwind

--- React/formularioReact/src/components/Hero.jsx
+++ React/formularioReact/src/components/Hero.jsx
@@ -3,7 +3,7 @@ export const Hero = () => {
   return (
     <div className="text-center mt-20 mb-8">
       <img className="mx-auto mb-8 w-14" src={logo} alt="Imagen de logo" />
-      <h1>¡Lanzaremos pronto!</h1>
+      <h1 className="text-[22px] ">¡Lanzaremos pronto!</h1>
       <p>Suscríbete y recibe una notificación</p>
     </div>
   );
