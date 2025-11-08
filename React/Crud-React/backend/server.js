19a207b8d565ee519c35ac45bee034bd910d2388
Emerson Reyes <reyesemerson643@gmail.com>
2025-11-07 19:59:40 -0500

    Backend

---
+++ React/Crud-React/backend/server.js
@@ -0,0 +1,46 @@
+//importaciones 
+//framework de node.js para crear el servidor
+import express from "express";
+//es para que no bloquee las peticiones
+import cors from "cors";
+//Es la libreria que conecta node.js con postgres
+import pkg from "pg";
+//sirve para las variables de entorno como el user la contraseña el puerto etc
+import dotenv from "dotenv";
+
+const {Pool} = pkg;
+
+//configuracion
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password :process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port : process.env.DB_PORT
+})
+
+
+app.get("/", (req, res) =>{
+    res.send("Servidor funcionando")
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{ console.log(`El servidor esta escuchando el puerto ${PORT}`)})
+console.log(process.env.DB_USER);
+
+
+app.get("/contactos" , async (req,res)=>{
+    try {
+        const result = await pool.query("select * from contactos")
+        res.json(result.rows)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error al obtener los contactos")
+    }
+})
